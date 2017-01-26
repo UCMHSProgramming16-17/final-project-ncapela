3e6283a415e91814d65141d11e7a791809cb83c9
Nelson Capela <ncapela@ucvts.org>
2017-01-26 19:05:53 +0000

    completed one graph and made csv file a readable csv file

--- soccerinfo.py
+++ soccerinfo.py
@@ -1,3 +1,13 @@
+import csv
 import pandas as pd
+import bokeh
 
 df = pd.read_csv("soccerinfo.csv")
+
+from bokeh.charts import Bar, output_file, save
+
+output_file("soccerinfo.html")
+
+rating = Bar(df, 'Team', 'Rating', title="Best Rated Teams")
+
+save(rating)
